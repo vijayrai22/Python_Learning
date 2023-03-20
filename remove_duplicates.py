bd1750c322bb64289c67d1f0bdaf08fb23d3aec6
Vijay <Vijaykumarrai22@gmail.com>
2023-03-20 19:56:10 +0530

    to remove duplicates from any list

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -4,3 +4,18 @@ for num in numbers:
     if num not in unique:
         unique.append(num)
 print(unique)
+
+# numbers= [5,2,1,7,4,5]
+# numbers.append(20) #to append any number in last
+# numbers.insert(0,10) #to to insert any number at given index
+# numbers.remove(5) #to remove any specified number
+# numbers.clear()  #to remove all the numbers from the list
+# numbers.pop() #to remove number from last
+# print(numbers.index(1)) # to find the index of given number
+# print(50 in numbers) #to find whether the given number is present or not it gives boolean value
+# print(numbers.count(5)) #to find the count or how many times the number is present
+# numbers.sort() # to sort the list ascending order
+# numbers.reverse() # to sort the list in descending order
+# numbers2= numbers.copy() #to copy the value into another list
+# numbers.append(10)
+# print(numbers2)
