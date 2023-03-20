28d93b519b71b0aec9d3622a976bba691928e330
Vijay <Vijaykumarrai22@gmail.com>
2023-03-20 23:32:43 +0530

    Basic emoji convertor using D ictionay

---
+++ emoji_convertor_using_python.py
@@ -0,0 +1,11 @@
+#emoji convertor using dictionay
+messege = input(">")
+words= messege.split(' ')
+emojis = {
+    ":)": "😁",
+    ":(": "😢"
+}
+output= ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
